458ca56c42f62c03e1b34cd54702a56f597ed410
Jakob Stendahl <jakob.stendahl@outlook.com>
2017-09-28 15:48:48 +0200

    Startet på TD :)

---
+++ td/Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed;
+	public float initialHp;
+	public Transform pathWay;
+
+	List<Vector3> waypoints = new List<Vector3>();
+	Vector3 currentWaypointPosition;
+	int currentWaypoint = 0;
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform child in pathWay) {
+			waypoints.Add (child.position);
+		}
+		currentWaypointPosition = new Vector3(waypoints [currentWaypoint].x, 0.604f, waypoints[currentWaypoint].z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position == currentWaypointPosition && currentWaypoint < waypoints.Count - 1) {
+			currentWaypoint += 1;
+			currentWaypointPosition = new Vector3(waypoints [currentWaypoint].x, 0.604f, waypoints[currentWaypoint].z);
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, currentWaypointPosition, speed * Time.deltaTime);
+	}
+		
+}
