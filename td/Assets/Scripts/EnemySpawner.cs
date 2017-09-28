4ede219d8afaee25f51177843939ab9d9a1b9b70
Jakob Stendahl <jakob.stendahl@outlook.com>
2017-09-29 01:09:20 +0200

    Added pan and zoom (Pan is ugly still)

--- td/Assets/Scripts/EnemySpawner.cs
+++ td/Assets/Scripts/EnemySpawner.cs
@@ -6,27 +6,20 @@ public class EnemySpawner : MonoBehaviour {
 
 	public Enemy enemyPrefab;
 	public Transform pathWay;
+	public Transform gameWorld;
 
 	int wave;
 	int next = 1;
 	int n = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		n++;
 
-
 		if (n == next) {
-			Debug.Log ("Spawn");
 			n = 0;
 			next = (int)Random.Range (50, 400);
 
-			Enemy newEnemy = Instantiate (enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+			Enemy newEnemy = Instantiate (enemyPrefab, new Vector3(0, 0, 0), Quaternion.identity, gameWorld);
 			newEnemy.GetComponent <Enemy> ().pathWay = pathWay;
 			newEnemy.GetComponent <Enemy> ().speed = Random.Range (0.3f, 1.2f);
 		}
