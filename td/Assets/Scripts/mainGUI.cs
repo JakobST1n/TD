c7d2d5cdb20d42a2014ff99d2fdffae68657e02e
Jakob Stendahl <jakob.stendahl@outlook.com>
2017-10-01 19:15:40 +0200

    Started on settings menu

--- td/Assets/Scripts/mainGUI.cs
+++ td/Assets/Scripts/mainGUI.cs
@@ -7,10 +7,13 @@ public class mainGUI : MonoBehaviour {
 
 	GameObject pnlMenu;
 	GameObject pnlSidebar;
+	GameObject pnlSettings;
 	RectTransform pnlSidebarTransform;
 	Button btnToggleSidebar;
 	Button btnPauseGame;
 	Button btnResumeGame;
+	Button btnExitGame;
+	Button btnSettings;
 
 	bool sidebarExpanded;
 	float[] sidebarStates = new float[2] {0f, -202.4f};  // The x position of the sidebar expanded or collapsed
@@ -21,19 +24,26 @@ public class mainGUI : MonoBehaviour {
 		/* Panels */
 		pnlMenu = transform.Find ("menu").gameObject;
 		pnlSidebar = transform.Find ("sidebarWrapper").gameObject;
+		pnlSettings = transform.Find ("settings").gameObject;
 		pnlSidebarTransform = pnlSidebar.GetComponent <RectTransform> ();
 
 		/* Buttons */
 		btnToggleSidebar = pnlSidebar.transform.Find("toggleSidebar").gameObject.GetComponent <Button> ();
 		btnPauseGame = pnlSidebar.transform.Find ("pauseGame").gameObject.GetComponent <Button> ();
 		btnResumeGame = pnlMenu.transform.Find ("resumeGame").gameObject.GetComponent <Button> ();
+		btnExitGame = pnlMenu.transform.Find ("exitGame").gameObject.GetComponent <Button> ();
+		btnSettings = pnlMenu.transform.Find ("settings").gameObject.GetComponent <Button> ();
 		if (btnToggleSidebar != null) { btnToggleSidebar.onClick.AddListener (toggleSidebarHandler); }
 		if (btnPauseGame != null) { btnPauseGame.onClick.AddListener (pauseGameHandler); }
 		if (btnResumeGame != null) { btnResumeGame.onClick.AddListener (btnResumeGameHandler); }
+		if (btnExitGame != null) { btnExitGame.onClick.AddListener (btnExitGameHandler); }
+		if (btnSettings != null) { btnSettings.onClick.AddListener (btnSettingsHandler); }
+
 
 		/* Set up initial states */
 		updateSidebarPosandBtn ();
 		pnlMenu.SetActive (false);
+		pnlSettings.SetActive (false);
 	}
 
 	void toggleSidebarHandler() {
@@ -58,6 +68,17 @@ public class mainGUI : MonoBehaviour {
 		Time.timeScale = 1.0F;
 		btnToggleSidebar.interactable = true;
 		btnPauseGame.interactable = true;
+	}
+
+	void btnExitGameHandler() {
+		/* handler for btnExitGame */
+		Application.Quit ();
+	}
+
+	void btnSettingsHandler() {
+		/* handler for btnSettings */
+		pnlMenu.SetActive (false);
+		pnlSettings.SetActive (true);
 	}
 
 	void updateSidebarPosandBtn() {
